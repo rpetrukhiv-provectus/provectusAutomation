d2698178ed3731152bce84944e5cc176292c7487
rpetrukhiv-provectus <rpetrukhiv@provectus.com>
2022-06-14 09:17:30 +0300

    Homework 4 Task for advanced level is not finished yet

---
+++ src/homeWorkFour/randomArray.java
@@ -0,0 +1,20 @@
+package homeWorkFour;
+
+import java.util.Random;
+
+public class randomArray {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int i = random.nextInt(100);
+        int[] array = new int[100];
+
+        for (; i <= array.length;i++){
+            if (i % 2 == 0){
+                System.out.println(i);
+            }
+            else {
+                System.out.println("0");
+            }
+        }
+    }
+}
---
+++ src/homeWorkFour/tableOfValues.java
@@ -0,0 +1,11 @@
+package homeWorkFour;
+
+public class tableOfValues {
+    public static void main(String[] args) {
+        int sin = 0;
+        for (; sin<=360; sin=sin+10){
+
+            System.out.println(sin);
+        }
+    }
+}
